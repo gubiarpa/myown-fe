76961c9ce909f3a5de083830eba4c8ca128bff7d
gubiarpa <billy.arredondo@pucp.pe>
2021-03-19 21:07:30 -0500

    app view

--- src/App.vue
+++ src/App.vue
@@ -37,11 +37,36 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-app-bar
+      app
+      dark
+      src="@/assets/background.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
 
-    <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-title>Avengers of the end time</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home pa-6">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In facilis expedita, dignissimos consequuntur temporibus, ipsa ducimus ipsam iste dolorem, debitis obcaecati officia saepe minus sint? Officiis ducimus quos enim maxime accusantium impedit repellendus dolorum magni, error corrupti inventore quo cumque nostrum quis voluptate aliquid ex in facere ipsam, ut commodi esse amet delectus illum? Inventore cumque ad incidunt ea totam!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In facilis expedita, dignissimos consequuntur temporibus, ipsa ducimus ipsam iste dolorem, debitis obcaecati officia saepe minus sint? Officiis ducimus quos enim maxime accusantium impedit repellendus dolorum magni, error corrupti inventore quo cumque nostrum quis voluptate aliquid ex in facere ipsam, ut commodi esse amet delectus illum? Inventore cumque ad incidunt ea totam!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In facilis expedita, dignissimos consequuntur temporibus, ipsa ducimus ipsam iste dolorem, debitis obcaecati officia saepe minus sint? Officiis ducimus quos enim maxime accusantium impedit repellendus dolorum magni, error corrupti inventore quo cumque nostrum quis voluptate aliquid ex in facere ipsam, ut commodi esse amet delectus illum? Inventore cumque ad incidunt ea totam!</p>
   </div>
 </template>
 
